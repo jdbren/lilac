8369fbfdfdc8428f5566d589421fab43236f07ab
jdb360 <jdb360@georgetown.edu>
2024-07-07 12:53:42 -0400

    refactor gdt

--- kernel/arch/x86/include/gdt.h
+++ kernel/arch/x86/include/gdt.h
@@ -1,7 +1,20 @@
-// Copyright (C) 2024 Jackson Brenneman
-// GPL-3.0-or-later (see LICENSE.txt)
-#ifndef x86_GDT_H
-#define x86_GDT_H
+#ifndef __GDT_H
+#define __GDT_H
+
+#include <asm/gdt.h>
+
+#define GDT_ENTRY(base, limit, access, flags) \
+( \
+    (limit & 0xff)        << 8 * 0 | \
+    (limit >> 8 & 0xff)   << 8 * 1 | \
+    (limit >> 16 & 0x0f)  << 8 * 6 | \
+    (base & 0xff)         << 8 * 2 | \
+    (base >> 8 & 0xff)    << 8 * 3 | \
+    (base >> 16 & 0xff)   << 8 * 4 | \
+    (base >> 24 & 0xff)   << 8 * 7 | \
+    (access)              << 8 * 5 | \
+    (flags << 4)          << 8 * 6   \
+)
 
 void gdt_init(void);
 void set_tss_esp0(u32 esp0);
