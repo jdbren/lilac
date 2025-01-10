cd65c25166dbff79688cf7426ab6c6e7c88e29e0
Jackson Brenneman <jdb360@georgetown.edu>
2025-01-09 19:10:00 -0500

    fix tabs

---
+++ libc/string/strdup.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#if defined(__is_libk)
+#include <lilac/mm/kmalloc.h>
+#endif
+
+char *strdup(const char *str)
+{
+#if defined(__is_libk)
+    size_t len = strlen(str) + 1;
+    char *new = kmalloc(len);
+    if (!new)
+        return NULL;
+    return strncpy(new, str, len);
+#else
+    return NULL;
+#endif
+}
