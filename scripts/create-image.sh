73675a235025daf0ef33af7db5fe66c12d029414
Jackson Brenneman <jdb360@georgetown.edu>
2025-02-26 21:46:59 -0500

    add script

---
+++ scripts/create-image.sh
@@ -0,0 +1,6 @@
+dd if=/dev/zero of=/home/jack/src_code/lilac/uefi.img bs=512 count=93750
+losetup --offset 1048576 --sizelimit 46934528 /dev/loop0 uefi.img
+mkfs.fat -F 32 /dev/loop0
+mount /dev/loop0 /mnt
+umount /mnt
+losetup -d /dev/loop0
