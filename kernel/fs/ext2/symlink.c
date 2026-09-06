3acf1604cf8ae69f3568b0c2ad1ab7a8cec16d4c
Jackson Brenneman <jdb360@georgetown.edu>
2026-09-06 09:36:01 -0400

    fix symlink memory leak

--- kernel/fs/ext2/symlink.c
+++ kernel/fs/ext2/symlink.c
@@ -11,16 +11,24 @@ const char * simple_get_link(struct dentry *dentry, struct inode *inode)
 const char * ext2_get_link(struct dentry *dentry, struct inode *inode)
 {
     struct ext2_inode_info *ext2_inode = EXT2_I(inode);
+    if (inode->i_link)
+        return inode->i_link;
+
     if (ext2_inode_is_fast_symlink(inode)) {
         return (char*)ext2_inode->i_data;
     } else {
-        struct blkio_desc *bh = sb_bread(inode->i_sb, ext2_inode->i_data[0]);
+        struct blkio_desc *bh = sb_bread(inode->i_sb, le32_to_cpu(ext2_inode->i_data[0]));
         if (IS_ERR_OR_NULL(bh))
             return bh ? ERR_CAST(bh) : ERR_PTR(-EIO);
         nd_terminate_link(bh->b_data, inode->i_size, inode->i_sb->s_blocksize - 1);
-        inode->i_link = strdup((const char*)bh->b_data);
+        char *link = strdup((const char*)bh->b_data);
+        if (!link) {
+            bdrop(bh);
+            return ERR_PTR(-ENOMEM);
+        }
         bdrop(bh);
-        return inode->i_link ? inode->i_link : ERR_PTR(-ENOMEM);
+        inode->i_link = link;
+        return inode->i_link;
     }
 }
 
